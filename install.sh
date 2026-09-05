c3d3ba2d6710fb9396df7eb345aaf5bb29e00ada
Isaac Sikkema <isaacsikkema@gmail.com>
2026-09-04 23:05:41 -0600

    Fix install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,18 @@ sudo apt-get update
 sudo apt-get install -y incus
 
 sudo incus admin init --minimal
-sudo incus network create starter-home-net ipv4.address=10.50.0.1/24 ipv6.address=none
-UID="$(id -u)"
-sudo incus project set user-$UID restricted.networks.access=starter-home-net
+sudo incus network create starter-net ipv4.address=10.50.0.1/24 ipv6.address=none
 sudo gpasswd --add "$USER" incus
+sg incus -c 'incus list'
+sudo incus profile device remove default eth0 --project "user-$UID"
+sudo incus project set user-$UID restricted.networks.access=starter-net
+sudo incus profile device add default eth0 --project "user-$UID" nic network=starter-net name=eth0
 
 echo "Installing uv..."
 wget -qO- https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 
 echo "Installing starter-home..."
-uv pip install .
+uv tool install .
 
 echo "Installed!"
