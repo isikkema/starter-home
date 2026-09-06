f9397c97576017230e0068de99792f73320ef942
Isaac Sikkema <isaacsikkema@gmail.com>
2026-09-05 19:01:57 -0600

    Reliably find starter-home dir

--- src/starter_home/files.py
+++ src/starter_home/files.py
@@ -1,3 +1,35 @@
+import json
+from importlib.metadata import distribution
 from pathlib import Path
+from urllib.parse import unquote, urlparse
 
-ROOT = Path(__file__).resolve().parent.parent.parent
+
+def get_root() -> Path:
+    dist = distribution("starter-home")
+    direct_url = None
+    if dist.files is None:
+        raise RuntimeError("Could not find starter-home directory")
+
+    for file in dist.files:
+        if str(file).endswith("direct_url.json"):
+            direct_url = file
+            break
+
+    if direct_url is None:
+        raise RuntimeError("Could not find starter-home directory")
+
+    with open(direct_url.locate(), "r") as f:
+        data = json.load(f)
+
+    if "dir_info" not in data:
+        raise RuntimeError("Not installed from a local directory")
+
+    return Path(unquote(urlparse(data["url"]).path)).resolve()
+
+
+ROOT = get_root()
+
+BASE_CONFIG = ROOT / "virtual-machine" / "base_config.json"
+CUSTOM_CONFIG = ROOT / "virtual-machine" / "custom_config.json"
+
+HOST_STORAGE = ROOT / "host-storage"
